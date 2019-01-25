26f7ad35d18cfa7cc7d82f8836eea4e64c301bce
turlir <iljaosincev@gmail.com>
2019-01-26 00:29:00 +0500

    first commit

---
+++ app/src/test/java/tinkoff/turlir/com/points/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package tinkoff.turlir.com.points
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun simpleTest() {
+        assertEquals(4, 2 + 2)
+    }
+}
