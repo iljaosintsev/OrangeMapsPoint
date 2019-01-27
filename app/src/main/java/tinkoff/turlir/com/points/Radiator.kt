fe52a6c868663e17ca3cde2610c7245aa5947d22
turlir <iljaosincev@gmail.com>
2019-01-28 01:26:06 +0500

    Radiator
    add android-maps-utils

---
+++ app/src/main/java/tinkoff/turlir/com/points/Radiator.kt
@@ -0,0 +1,38 @@
+package tinkoff.turlir.com.points
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import javax.inject.Inject
+
+/**
+ * @see [Wiki](https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Resolution_and_Scale)
+ * @see [Wiki](https://wiki.openstreetmap.org/wiki/Zoom_levels)
+ */
+class Radiator @Inject constructor() {
+
+    private val equator = 40075.016686 // km
+    private val c = equator * 1000 / 256 // meter/pixel
+
+    /**
+     * @param lat – широта на отсчетной точке
+     * @param zoom – масштаб карты
+     * @param height – длина в DIP
+     * @return расстояние в метрах
+     */
+    fun radius(lat: Double, zoom: Double, height: Double): Int {
+        val meterPerPixel = c *
+                Math.cos(lat * Math.PI / 180) /
+                Math.pow(2.0, zoom)
+
+        return Math.round(meterPerPixel * height).toInt()
+    }
+
+    /**
+     * @param from – отсечетная точка
+     * @param to – конченая точка
+     * @return расстояние в метрах между [from] и [to]
+     */
+    fun radius(from: LatLng, to: LatLng): Int {
+        return Math.round(SphericalUtil.computeDistanceBetween(from, to)).toInt()
+    }
+}
---
+++ app/src/test/java/tinkoff/turlir/com/points/RadiatorTest.kt
@@ -0,0 +1,37 @@
+package tinkoff.turlir.com.points
+
+import com.google.android.gms.maps.model.LatLng
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class RadiatorTest {
+
+    private lateinit var radiator: Radiator
+
+    @Before
+    fun setup() {
+        radiator = Radiator()
+    }
+
+    @Test
+    fun computeDistanceRandom() {
+        val top = LatLng(57.024977692567134, 61.45873457193374)
+        val bottom = LatLng(57.01069337050285, 61.45873457193374)
+
+        val distance = radiator.radius(top, bottom)
+        assertEquals(1588, distance)
+    }
+
+    @Test
+    fun computePixelRandom() {
+        val lat = 57.024977692567134
+        val zoom = 15.0
+        val pixels = 2140
+        val dp = 3.5
+        val dip = pixels / dp
+
+        val distance = radiator.radius(lat, zoom, dip)
+        assertEquals(1590, distance)
+    }
+}
