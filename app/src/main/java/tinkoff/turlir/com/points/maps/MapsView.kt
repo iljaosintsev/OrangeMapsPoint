31d028b5dbb6f3b2f535c4d0fe806e92824b4e4c
turlir <iljaosincev@gmail.com>
2019-01-29 20:28:51 +0500

    moxy default strategy for MapsView

--- app/src/main/java/tinkoff/turlir/com/points/maps/MapsView.kt
+++ app/src/main/java/tinkoff/turlir/com/points/maps/MapsView.kt
@@ -2,8 +2,11 @@ package tinkoff.turlir.com.points.maps
 
 import android.location.Location
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import tinkoff.turlir.com.points.storage.Partner
 
+@StateStrategyType(value = AddToEndSingleStrategy::class)
 interface MapsView: MvpView {
 
     fun moveToLocation(location: Location)
